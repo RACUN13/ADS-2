d5ac8892d4687c7410b7e48b9004813d21632446
RACUN13 <125272048+RACUN13@users.noreply.github.com>
2023-03-31 00:56:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -12,38 +12,38 @@ return value;
 }
 
 uint64_t fact(uint16_t n) {
-if (n < 0)
-    return 0;
-  if (n == 0)
-    return 1;
-  else
-    return n * fact(n - 1);
+    if (n < 0)
+        return 0;
+    if (n == 0)
+        return 1;
+    else
+        return n * fact(n - 1);
 }
 
 double calcItem(double x, uint16_t n) {
-return(pown(x, n) / fact(n));
+  return pown(x, n) / fact(n);
 }
 
 double expn(double x, uint16_t count) {
-double exp = 1;
-for (int i = 1; i <= count; i++) {
-exp += calcItem(x, i);
-}
-return exp;
+  double exp = 1;
+  for (uint16_t i = 1; i <= count; i++) {
+    exp += calcItem(x, i);
+  }
+  return exp;
 }
 
 double sinn(double x, uint16_t count) {
-double sin = 0;
-for (int i = 1; i <= count; ++i) {
-sin += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
-}
-return sin;
+  double sin = 0;
+  for (uint16_t i = 1; i <= count; i++) {
+    sin += pown(-1, i - 1) * calcItem(x, 2 * i - 1);
+  }
+  return sin;
 }
 
 double cosn(double x, uint16_t count) {
-double cos = 1;
-for (int i = 2; i <= count; i++) {
-cos += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
-}
-return cos;
+  double cos = 1;
+  for (uint16_t i = 2; i <= count; i++) {
+    cos += pown(-1, i - 1) * calcItem(x, 2 * i - 2);
+  }
+  return cos;
 }
